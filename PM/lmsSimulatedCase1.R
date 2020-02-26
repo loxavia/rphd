d9cda422910ff6903dc6abca59af80cbe1b3d053
Dhiraj Upadhyaya <dup1966@gmail.com>
2020-02-26 06:44:50 +0530

    a

--- PM/lmsSimulatedCase1.R
+++ PM/lmsSimulatedCase1.R
@@ -1,37 +1,47 @@
 #simulated LMS data for PM
 
-library(processanimateR)
-library(eventdataR)
+#libraries
 library(bupaR)
 library(dplyr)
+library(eventdataR)
+library(edeaR)
+library(processanimateR)
 library(processmapR)
+library(processmonitR)
 
-
+#create some activities
 (activities = c('Forum', 'PPT', 'Assignment','Quiz', 'Game' ,'Survey'))
 (rollno = paste('S',1:50,sep='-'))
-e1 <- expand.grid(rollno, activities)
+
+(e1 <- expand.grid(rollno, activities))
 head(e1)
 names(e1) <- c('rollno','activity')
+
+#sample dates of month
 e1$timestamp = as.POSIXct(as.Date(paste('2020','2',sample(1:30, size=nrow(e1), replace=T),sep='-')))
 head(e1)
 
 #student Profile----
 (rollno = paste('S',1:50,sep='-'))
+(sname = paste('Student-',1:50, sep=''))
 (gender = sample(c('M','F'), size=50, replace=T, prob=c(.7,.3)))
 (finalMarks = trunc(runif(50,min=60,max=500)))
-(students = data.frame(rollno, gender, finalMarks, stringsAsFactors = F))
-?merge
+(students = data.frame(rollno, sname, gender, finalMarks, stringsAsFactors = F))
 head(students)
 names(students) ; names(e1)
 (e2 <- merge(x=students, y=e1, all.y=T ))
 
-  
 #select fraction of rows randomly 
 e3 <- e2 %>% sample_frac(.8)
+
 #create bupaR object -----
 events1 <- bupaR::simple_eventlog(eventlog = e3, case_id = 'rollno', activity_id = 'activity', timestamp = 'timestamp')
 
 events1
+head(events1)
+str(events1)
+summary(events1)
+
 #how many activities----
 activity_frequency(events1)
 #freq of participation in activities----
@@ -44,9 +54,9 @@ processmapR::process_map(events1)
 #Animate Process-----
 ap1 <- animate_process(events1)
 ap1
-animate_process(events1, legend=T, mode='absolute')
-animate_process(events1, legend=T, mode='relative')
-animate_process(events1, legend=T, mode='relative', duration=10,  mapping = token_aes(color = token_scale("red"), size = token_scale(10)))
+animate_process(events1, legend=T, mode='absolute', duration=10)
+animate_process(events1, legend=T, mode='relative', duration=10)
+animate_process(events1, legend=T, mode='relative', duration=10,mapping = token_aes(color = token_scale("red"), size = token_scale(10)))
 names(events1)
 str(events1)
 events1$gender = factor(events1$gender)
@@ -62,9 +72,7 @@ animate_process(events1,  legend = "color",   mapping = token_aes(color = token_
 animate_process(events1,  legend = "size",   mapping = token_aes(size = token_scale(12), shape='rect'), duration=10)
 animate_process(events1, legend='size', mapping = token_aes(size = token_scale(15), shape = "rect"), duration=10)
 
-animate_process(events1,  mapping = token_aes(co = token_scale(attribute='gender', range=c('red','blue'))), duration=10)
-
-
+animate_process(events1,  mapping = token_aes(color = token_scale(attribute='gender', range=c('red','blue'))), duration=10, legend=T)
 
 animate_process(events1, legend=T, mode='relative', duration=10,  mapping = token_aes(color = token_scale('marks'), scale = 'linear'))
 
@@ -100,24 +108,26 @@ events1
 events1 %>%  filter_trace_length(interval = c(2, 5)) %>% trace_length(units = "min")
 events1 %>%  filter_trace_length(percentage = 0.5) %>%  trace_length()
 #traces which have PPT
-events1 %>%  filter_activity_presence("PPT") %>% traces
+events1 %>%  filter_activity_presence("PPT") %>% bupaR::traces()
 #traces which do not have PPT
-events1 %>% filter_activity_presence("PPT", reverse = T) %>%  traces
-events1 %>%  filter_activity_presence(c("PPT", "Quiz"), method = "all")  %>%   traces
-events1 %>%  filter_activity_presence(c("PPT", "Quiz", reverse=T), method = "all")  %>% traces
+events1 %>% filter_activity_presence("PPT", reverse = T) %>%  bupaR::traces()
+events1 %>%  filter_activity_presence(c("PPT", "Quiz"), method = "all")  %>%   bupaR::traces()
+events1 %>%  filter_activity_presence(c("PPT", "Quiz", reverse=T), method = "all")  %>% bupaR::traces()
 #one of the activities
-events1 %>%  filter_activity_presence(c("PPT", "Quiz"), method = "one_of")  %>% traces
-
+events1 %>%  filter_activity_presence(c("PPT", "Quiz"), method = "one_of")  %>% bupaR::traces()
+?traces
+bupaR::traces(events1)
 #either PPT or Quiz was not there--
-events1 %>%  filter_activity_presence(c("PPT", "Quiz", reverse=T), method = "one_of")  %>% traces
+events1 %>%  filter_activity_presence(c("PPT", "Quiz", reverse=T), method = "one_of")  %>% bupaR::traces()
 
 #none of them: students who did not take none of these
-events1 %>% filter_activity_presence(c("PPT", "Game"), method = "none")  %>% traces
+events1 %>% filter_activity_presence(c("PPT", "Game"), method = "none")  %>% bupaR::traces()
+events1 %>%  filter_activity_presence(c("PPT", "Quiz", reverse=T), method = "all")  %>% bupaR::traces()
 
-
+events1 %>% filter_activity_presence('PPT')
 #start activities---
 #student who started with PPT---
-events1 %>% filter_endpoints(start_activities = "PPT") %>% traces
+events1 %>% filter_endpoints(start_activities = "PPT")
 #students who did not start with PPT
 events1 %>% filter_endpoints(start_activities = "PPT", reverse = T) %>% traces
 #ended with
@@ -125,13 +135,14 @@ events1 %>%  filter_endpoints(end_activities = "Quiz") %>% traces()
 #did not end with 
 events1 %>%  filter_endpoints(end_activities = "Quiz", reverse=T) %>% traces()
 #start and end with
-events1 %>%  filter_endpoints(start_activities = "Quiz", end_activities = "Forum") %>% traces()
-#map
 events1 %>%  filter_endpoints(start_activities = "Quiz", end_activities = "Forum") %>% process_map()
 
+#map
+events1 %>%  filter_endpoints(start_activities = "Quiz", end_activities = "Forum") %>% process_map()
+?process_map
 
 #Precedence-----
-events1 %>%  filter_precedence(antecedents = "PPT",  consequents = "Quiz",precedence_type = "directly_follows") %>% traces
+events1 %>%  filter_precedence(antecedents = "PPT",  consequents = "Quiz",precedence_type = "directly_follows") %>% bupaR::traces()
 #PPT follows Forum & Quiz - data/ traces
 events1 %>%  filter_precedence(antecedents = "PPT", consequents = c("Forum", "Quiz"),  precedence_type = "eventually_follows", filter_method = "all") %>%  traces
 #PPT follows Forum and Quiz both directly or later
@@ -145,20 +156,21 @@ events1 %>%  filter_precedence(antecedents = "PPT", consequents = c("Quiz", "Ass
 
 
 #Select 80% of the cases that share the most common traces
-events1 %>%  filter_trace_frequency(percentage = 0.8) %>% n_cases()
+events1 %>%  filter_trace_frequency(percentage = 0.8) %>% bupaR::n_cases()
 events1 %>%  filter_trace_frequency(percentage = 0.8) %>% process_map()
 events1 %>%  filter_trace_frequency(percentage = 0.5) %>% n_cases()
 
 #Or the cases of which the trace frequency is less than 25.
-events1 %>%  filter_trace_frequency(interval = c(0,25)) %>%   n_cases()
+events1 %>%  filter_trace_frequency(interval = c(3,5)) %>%   bupaR::n_cases()
 #????
-
+?filter_trace_frequency
 #Time Period---
-
-events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "start") %>% dotted_chart
-events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "complete") %>% dotted_chart
+?dotted_chart
+?filter_time_period
+events1 %>%  filter_time_period(interval = ymd(c(20200215, 20200220)), filter_method = "start")
+events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "complete") %>% bupaR::n_cases()
 events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "contained") %>% dotted_chart
-events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "intersecting") %>% dotted_chart 
+events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "intersecting") 
 events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "start") %>% dotted_chart(units='weeks')
 events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "start") %>% dotted_chart(units='hours')
 events1 %>%  filter_time_period(interval = ymd(c(20200210, 20200220)), filter_method = "start") %>% dotted_chart(units='hours', sort='end')
@@ -176,5 +188,64 @@ processmapR::trace_explorer(events1, coverage=.5)
 processmapR::trace_explorer(events1, n_traces=15)
 processmapR::trace_explorer(events1, n_traces=15, type='frequent')
 processmapR::trace_explorer(events1, n_traces=15, type='infrequent')
-
+plotly_trace_explorer(events1)
 ?trace_explorer
+
+
+events1 %>%  filter_activity_frequency(percentage = 1.0) %>% 
+  filter_trace_frequency(percentage = .80) %>%  process_map(render = F) %>% DiagrammeR::export_graph(file_name = 'E:/PMC/pm11.png',  file_type = 'PNG')
+
+events1 %>%   filter_activity_frequency(percentage = 1.0) %>% 
+  filter_trace_frequency(percentage = .80) %>%   precedence_matrix() %>%  plot()
+
+events1 %>% bupaR::act_unite(PPTquiz= c("PPT","Quiz"))
+events1 %>% bupaR::act_collapse(PPTquiz= c("PPT","Quiz"))
+?bupaR::act_collapse
+?bupaR::act_unite
+
+events1 %>% trace_coverage(level = "case") 
+events1 %>% trace_coverage(level = "case", append=T) %>% as.data.frame() %>% select(rollno, trace, absolute_case_trace_coverage)
+events1 %>% trace_coverage(level = "case", append_column = "relative") 
+
+library(bupaR)
+?group_by_activity
+events1 %>%  bupaR::group_by_case %>%   mutate(Attnd = any(eventContext == "PPT")) %>%   ungroup_eventlog()
+events1 %>% bupaR::group_by_activity() %>% bupaR::n_cases()
+events1 %>% filter( grepl("PP",activity)) %>% process_map()
+names(events1)
+events1 %>% arrange(desc(timestamp))
+events1 %>% slice_activities(1:5)
+events1 %>% slice_events(1:10)
+events1 %>%  group_by_case() %>% first_n(10) %>%  trace_explorer(coverage = .8)
+mapping(events1)
+events1 %>% activity_presence() %>%  plot
+events1 %>%  start_activities("resource-activity")
+events1 %>%  end_activities("resource-activity")
+events1 %>%   process_map(type = frequency("relative"))
+events1 %>%   process_map(type = frequency("absolute"))
+events1 %>%   process_map(type = frequency("absolute_case"))
+events1 %>%   process_map(type = frequency("relative_case"))
+
+events1 %>%   process_map(type = frequency("relative_case"), color_scale = "Purples")
+events1 %>%  process_map(performance(median, "days"))
+events1 %>%  process_map(performance(mean, "hours"))
+events1 %>%  process_map(type_nodes = frequency("relative_case"),    type_edges = performance(mean))
+events1 %>% group_by(activity) %>%   n_cases()
+#whether a case followed a frequent or infrequent path. 
+events1 %>% trace_coverage(level = "case", append = T, append_column = "relative") %>% mutate(frequent = relative_case_trace_coverage > 0.2) %>% filter(frequent=="FALSE") %>% select(sname, frequent)
+
+
+
+#
+activities(events1)
+activity_labels(events1)
+activity_id(events1)
+activity_instance_id(events1)
+arrange(events1)
+case_list(events1)
+?arrange
+group_by_case(events1) %>% first_n(2)
+group_by_case(events1) %>% last_n(1)
+lifecycles(events1)
+n_traces(events1)
+trace_list(events1)
