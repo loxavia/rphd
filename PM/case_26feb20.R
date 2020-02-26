e16b176e9b176539e6f1d16b937599782145baa2
Dhiraj Upadhyaya <dup1966@gmail.com>
2020-02-26 22:34:21 +0530

    a

--- PM/case_26feb20.R
+++ PM/case_26feb20.R
@@ -32,3 +32,34 @@ head(df)
 df1 = df
 df2 <- df1 %>% group_by(activity, case) %>% summarise(count=n()) %>% tibble::rowid_to_column("activity_instance2") %>% select(-c(count)) %>% merge(df1)
 df2
+
+event %>% process_map(type = frequency("absolute"))
+event %>% process_map(type = frequency("relative"))
+
+#------------dfnew-----
+link2='https://docs.google.com/spreadsheets/d/139d4RD4wnf4Qvp2mg2DCMWCOOemmiQM1NA9K7vn5Ak0/edit#gid=2038410307'
+
+newdf = as.data.frame(gsheet2tbl(link2))
+head(newdf)
+newdf
+names(newdf)
+str(newdf)
+newdf2 <- newdf %>% group_by(activity, case) %>% summarise(count=n()) %>% tibble::rowid_to_column("activity_instance") %>% select(-c(count)) %>% merge(newdf)
+newdf2
+
+event2 <- bupaR::eventlog(eventlog=newdf2, case_id='case', activity_id ='activity', activity_instance_id= 'activity_instance', lifecycle_id='lifecycle', timestamp = 'timestamp', resource_id ='resource')
+event2
+
+event2 %>% process_map()
+n_activities(event2)
+n_cases(event2)
+resource_map(event2)
+dotted_chart(event2)
+precedence_matrix(event2)
+precedence_matrix(event2) %>% plot()
+process_matrix(event2)  %>% plot()
+trace_explorer(event2)
+animate_process(event2, duration=10)
+
+event2 %>% process_map(type = frequency("absolute"))
+event2 %>% process_map(type = frequency("relative"))
